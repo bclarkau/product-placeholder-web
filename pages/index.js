37a8b408a38e8f098f2724dc1cdab2cf2d8e5c2b
Ben <benclark001@gmail.com>
2022-01-04 16:49:52 +1100

    test vercel deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 const Home = () => {
-  return <div className="flex justify-center items-center h-screen">Coming soon</div>
+  return <div className="flex justify-center items-center h-screen text-teal-500">Coming soon</div>
 }
 
 export default Home
