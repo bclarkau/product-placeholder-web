3f35ecb7a5623d7a7ccfe422debd3bb0f34f174d
Ben <benclark001@gmail.com>
2022-01-04 18:40:08 +1100

    style header product preview

---
+++ components/product.js
@@ -0,0 +1,12 @@
+export const Product = ({ id }) => {
+	return <div>
+		<div className="relative mb-2">
+			<img src={`http://localhost:3001/300?id=${id}&p=30`} alt="Product" />
+			<div className="absolute inset-0 rounded-lg bg-stone-100 -z-10" />
+		</div>
+		<div className="flex justify-between items-center">
+			<div className="text-stone-500 font-light">Product Name</div>
+			<div className="text-stone-800 font-bold text-lg">$20</div>
+		</div>
+	</div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { Product } from "../components/product"
 
 const Home = () => {
   return <div className="container mx-auto scroll-smooth">
@@ -6,29 +7,31 @@ const Home = () => {
       <title>Product image placeholders for e-commerce mockups</title>
     </Head>
 
-    <header className="flex">
+    <header className="flex justify-between">
 
-      <div className="w-2/5 pt-40">
-        <h1 className="text-orange-500 text-6xl font-bold mb-5">Product<br />Placeholders</h1>
-        <p className="mb-16">Transparent PNGs for quick e-commerce mockups</p>
+      <div className="pt-60">
+        <h1 className="text-orange-500 text-7xl font-bold mb-5">Product<br />Placeholders</h1>
+        <p className="mb-16 text-lg text-stone-700">Transparent product-ish PNGs for quick e-commerce mockups</p>
         <nav>
           <a href="#howto" className="inline-block px-10 py-5 text-white bg-orange-500 rounded">How to use</a>
           {/* <a href="#howto" className="inline-block px-10 py-5 text-white bg-orange-500 rounded">Images</a> */}
         </nav>
       </div>
 
-      <div className="w-3/5 grid grid-cols-2 p-10">
-        <div>
-          <img src="http://localhost:3001/300" alt="Product" />
-        </div>
-        <div>
-          <img src="http://localhost:3001/300" alt="Product" />
-        </div>
-        <div>
-          <img src="http://localhost:3001/300" alt="Product" />
-        </div>
-        <div>
-          <img src="http://localhost:3001/300" alt="Product" />
+      <div className="pt-20">
+        <div className="inline-grid grid-cols-2 gap-5">
+          <div>
+            <Product id={35} />
+          </div>
+          <div>
+            <Product id={2} />
+          </div>
+          <div>
+            <Product id={12} />
+          </div>
+          <div>
+            <Product id={17} />
+          </div>
         </div>
       </div>
 
