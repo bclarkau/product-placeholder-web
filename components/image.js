4816dc46dec4e47e889ed43575518c78569af31a
Ben <benclark001@gmail.com>
2022-01-05 12:28:52 +1100

    fix broken next images

--- components/image.js
+++ components/image.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export const ProductImage = ({ id, width = 200, height = 200, padding = 0 }) => {
-	return <Image src={`https://prd.place/${width}/${height}?id=${id}&p=${padding}`} alt="Product" />
+	return <Image src={`https://prd.place/${width}/${height}?id=${id}&p=${padding}`} alt="Product" {...{width, height}} />
 }
