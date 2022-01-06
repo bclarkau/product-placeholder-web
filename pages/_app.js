903f0cd3768765e5447e12962d743f8fbb79c65a
Ben <benclark001@gmail.com>
2022-01-06 18:03:49 +1100

    Add analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,12 @@ const App = ({ Component, pageProps }) => {
 			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff8300" />
 			<meta name="msapplication-TileColor" content="#ff8300" />
 			<meta name="theme-color" content="#ff8300" />
+
+			<script async src="https://cdn.panelbear.com/analytics.js?site=DaOAvmDeyxp"></script>
+			<script>
+				window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+				panelbear('config', { site: 'DaOAvmDeyxp' });
+			</script>
 		</Head>
 		<Component {...pageProps} />
 	</>
